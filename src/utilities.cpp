43b10756fe0e7dd50c5fc3348a5d36ff0f95f505
Ethan Scott <esa103@sfu.ca>
2025-07-17 18:18:44 -0700

    fixed missing include and missing function return types

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <format>
 #include "utilities.hpp"
 
 
-Utilities::extractSailingID(std::string& sailing_id, std::string& terminal, int& departure_day, int& departure_hour) {
+void Utilities::extractSailingID(std::string& sailing_id, std::string& terminal, int& departure_day, int& departure_hour) {
     // String stream
     std::stringstream string_stream(sailing_id);
 
@@ -16,8 +17,8 @@ Utilities::extractSailingID(std::string& sailing_id, std::string& terminal, int&
     string_stream >> departure_day >> dummy >> departure_hour; // Uses dummy to consume the 2nd dash
 }
 
-Utilities::createSailingID(const std::string& terminal, const int departure_day, const int departure_hour, std::string& output_sailing_id) {
+void Utilities::createSailingID(const std::string& terminal, const int departure_day, const int departure_hour, std::string& output_sailing_id) {
 
-    output_sailing_id = std::format("{}-{:02}-{:02}", terminal, departure_day, departure_hour)
+    output_sailing_id = std::format("{}-{:02}-{:02}", terminal, departure_day, departure_hour);
 
 }
