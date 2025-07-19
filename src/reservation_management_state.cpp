3faff8bc1f33fed8ad18f2465117aaf1d8d6dde8
karanpunni <kspunni@gmail.com>
2025-07-18 20:03:28 -0700

    Reservation state complete

--- src/reservation_management_state.cpp
+++ src/reservation_management_state.cpp
@@ -102,8 +102,6 @@ void ReservationManagementState::onExit()
 void ReservationManagementState::createReservation()
 {   
     std::string sailing_data;
-    // std::string license_plate;
-    // std::string phone_number;
 
       //Sailing ID (assuming format: 3 letters-2 digits-2 digits like "AHS-22-10")
     std::regex sailing_pattern("[A-Z]{3}-\\d{2}-\\d{2}");
@@ -127,27 +125,27 @@ void ReservationManagementState::createReservation()
     std::regex plate_pattern("[A-Z0-9]{3}-[A-Z0-9]{3}");
     continuouslyPromptForString("Please enter the licence plate of the vehicle: ", plate_pattern,s_vehicle.license_plate);
 
-    //Phone number (12-digit only digits)
-    std::regex phone_pattern("\\d{12}");
-    continuouslyPromptForString("Please enter the phone number of the owner: ", phone_pattern, s_vehicle.phone_number);
-
-    //Vehicle length [0–99.9]
-    continuouslyPromptForReal("Please enter the length of the vehicle [0-99.9]: ", 0, 99, s_vehicle.length);
-
-    //Vehicle height [0–9.9]
-    continuouslyPromptForReal("Please enter the height of the vehicle [0-9.9]: ", 0.0, 9.9, s_vehicle.height);
-
     m_database->getVehicleByID(s_vehicle.license_plate,s_vehicle,g_is_successful,g_outcome_message);
 
     if (!g_is_successful) {
+         //Phone number (12-digit only digits)
+        std::regex phone_pattern("\\d{12}");
+        continuouslyPromptForString("Please enter the phone number of the owner: ", phone_pattern, s_vehicle.phone_number);
+
+        //Vehicle length [0–99.9]
+        continuouslyPromptForReal("Please enter the length of the vehicle [0-99.9]: ", 0, 99, s_vehicle.length);
+
+        //Vehicle height [0–9.9]
+        continuouslyPromptForReal("Please enter the height of the vehicle [0-9.9]: ", 0.0, 9.9, s_vehicle.height);
+
         m_database->addVehicle(s_vehicle,s_vehicle.vehicle_id,g_is_successful,g_outcome_message);
         if (!g_is_successful) {
         std::cout << g_outcome_message << "\n\n";
         m_state_manager->selectNextState(States::ReservationManagementState);
         } 
     }
-    //---------------------------------------Fix if found vehicle no need to ask for other information-----------------------------------------------
-    char confirm;
+
+    char confirm; //to variable to take userinput to confirm the creation of reservation.
 
     //Confirmation
     continuouslyPromptForCharacter("Are you sure you want to create this new reservation [y/n]? ", {'y', 'n'}, confirm);
@@ -160,13 +158,62 @@ void ReservationManagementState::createReservation()
         m_state_manager->selectNextState(States::ReservationManagementState);
         } 
         std::cout << "\nNew reservation successfully created!\n";
+        
     } else {
         std::cout << "\nReservation Abort.\n";
     }
+    m_state_manager->selectNextState(States::MainMenuState);
 }
 
 // ----------------------------------------------------------------------------
 void ReservationManagementState::deleteReservation()
 {
+    std::string sailing_data;
+
+      //Sailing ID (assuming format: 3 letters-2 digits-2 digits like "AHS-22-10")
+    std::regex sailing_pattern("[A-Z]{3}-\\d{2}-\\d{2}");
+    continuouslyPromptForString("Please enter the ID of the sailing [TTT-dd-hh]: ", sailing_pattern,sailing_data); 
+
+    // Parse components
+    std::string terminal = sailing_data.substr(0, 3);
+    int day = std::stoi(sailing_data.substr(4, 2));
+    int hour = std::stoi(sailing_data.substr(7, 2));
+
+    m_database->getSailingByID(terminal, day, hour, s_sailing, g_is_successful, g_outcome_message);
+
+    if (!g_is_successful) {
+        std::cout << g_outcome_message << "\n\n";
+        m_state_manager->selectNextState(States::ReservationManagementState);
+    }    
+
+    //License plate (pattern A76-2H4)
+    std::regex plate_pattern("[A-Z0-9]{3}-[A-Z0-9]{3}");
+    continuouslyPromptForString("Please enter the licence plate of the vehicle: ", plate_pattern,s_vehicle.license_plate);
+
+    m_database->getVehicleByID(s_vehicle.license_plate,s_vehicle,g_is_successful,g_outcome_message);
+    if (!g_is_successful) {
+        std::cout << g_outcome_message << "\n\n";
+        m_state_manager->selectNextState(States::ReservationManagementState);
+    }
+
+    char confirm; //to variable to take userinput to confirm the creation of reservation.
+
+    //Confirmation
+    continuouslyPromptForCharacter("Are you sure you want to cancel this reservation [y/n]? ", {'y', 'n'}, confirm);
+
+    // Final message
+    if (confirm == 'y') {
+        m_database->removeReservation(s_sailing,s_vehicle,g_is_successful,g_outcome_message);
+        if (!g_is_successful) {
+        std::cout << g_outcome_message << "\n\n";
+        m_state_manager->selectNextState(States::ReservationManagementState);
+        } 
+        std::cout << "\nReservation successfully cancelled!\n";
+        
+    } else {
+        std::cout << "\nReservation cancelling operation aborted!\n";
+    }
+    m_state_manager->selectNextState(States::MainMenuState);
+    
     
 }
